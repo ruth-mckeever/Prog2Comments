90c00c64c02d3044980966aa9b0c74a71d5fc50c
Ruth McKeever <ruth.mckeever@dkit.ie>
2024-10-14 20:36:51 +0100

    Add project files.

---
+++ SanitizeService.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+using System.Xml.Serialization;
+
+namespace Prog2Comments
+{
+    public class SanitizeService
+    {
+        static readonly string SanitizeURL = "http://www.purgomalum.com/service/xml";
+        public static string? SanitizeText(string rawText)
+        {
+            var client = new RestClient(SanitizeURL);
+            var request = new RestRequest();
+            request.AddParameter("text", rawText);
+            var response = client.Execute(request);
+
+            if (!string.IsNullOrWhiteSpace(response.Content))
+            {
+                var serializer = new XmlSerializer(typeof(PurgoMalum));
+                using (StringReader sr = new(response.Content))
+                {
+                    PurgoMalum? sanitized = (PurgoMalum?)serializer.Deserialize(sr);
+                    return sanitized?.result;
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ UserComment.cs
@@ -0,0 +1,54 @@
+﻿namespace Prog2Comments
+{
+    public class UserComment
+    {
+        private string rawComment;
+        private string sanitizedComment = "";
+
+        public string RawComment
+        {
+            get { return rawComment; }
+            set { rawComment = value; }
+        }
+
+        public string SanitizedComment
+        {
+            get { return sanitizedComment; }
+            set { sanitizedComment = value; }
+        }
+        public UserComment(string rawComment)
+        {
+            this.rawComment = rawComment;
+            sanitizedComment = SanitizeService.SanitizeText(rawComment);  //TODO: will sanitize this here
+        }
+
+    }
+
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.purgomalum.com")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.purgomalum.com", IsNullable = false)]
+    public partial class PurgoMalum
+    {
+
+        private string resultField;
+
+        /// <remarks/>
+        public string result
+        {
+            get
+            {
+                return this.resultField;
+            }
+            set
+            {
+                this.resultField = value;
+            }
+        }
+    }
+
+
+}
